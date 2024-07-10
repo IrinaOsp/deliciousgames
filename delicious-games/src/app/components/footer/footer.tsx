ea183d013de4170caf2adfbfdc6007db7526bd06
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-07-10 15:42:29 +0200

    fix footer styles

--- delicious-games/src/app/components/footer/footer.tsx
+++ delicious-games/src/app/components/footer/footer.tsx
@@ -10,8 +10,8 @@ import {
 
 export default function Footer() {
   return (
-    <footer className="w-screen bg-zinc-100">
-      <div className="flex lg:flex-nowrap container mx-0 pt-[30px] px-5 pb-[10px] lg:mx-auto justify-between items-start lg:min-w-max lg:max-w-7xl lg:gap-5 ">
+    <footer className="w-screen bg-zinc-100 mt-auto mb-0">
+      <div className="flex flex-wrap gap-5 sm:flex-nowrap container mx-0 pt-[30px] px-5 pb-[10px] lg:mx-auto justify-between items-start lg:min-w-max lg:max-w-7xl lg:gap-5 ">
         {FOOTER_MENU_LINKS.map(({ section, links }) => (
           <div key={section} className="mb-5">
             <h3
@@ -32,19 +32,19 @@ export default function Footer() {
         <div className="flex gap-[10px]">
           <Link
             href={SocialNetworksLinks.FACEBOOK}
-            className="flex justify-center items-center bg-sky-800 size-10 rounded-full text-xl"
+            className="flex justify-center items-center bg-sky-800 size-10 rounded-full text-xl hover:bg-pink-600"
           >
             <FontAwesomeIcon icon={faFacebookF} style={{ color: "white" }} />
           </Link>
           <Link
             href={SocialNetworksLinks.TWITTER}
-            className="flex justify-center items-center bg-sky-800 size-10 rounded-full text-xl"
+            className="flex justify-center items-center bg-sky-800 size-10 rounded-full text-xl hover:bg-pink-600"
           >
             <FontAwesomeIcon icon={faTwitter} style={{ color: "white" }} />
           </Link>
           <Link
             href={SocialNetworksLinks.INSTAGRAM}
-            className="flex justify-center items-center bg-sky-800 size-10 rounded-full text-xl"
+            className="flex justify-center items-center bg-sky-800 size-10 rounded-full text-xl hover:bg-pink-600"
           >
             <FontAwesomeIcon icon={faInstagram} style={{ color: "white" }} />
           </Link>
