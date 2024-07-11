e35ae4bc37134b5b3e30d72a2e5b3171c0855d3b
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-07-11 11:42:57 +0200

    add contact data to contact page

---
+++ delicious-games/src/app/contact/contactCards/contactCards.tsx
@@ -0,0 +1,75 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDollarSign,
+  faEnvelope,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
+import { COMPANY_ADDRESS, EMAILS, INVOICE_DATA } from "@/data/contactsData";
+
+export default function ContactCards() {
+  return (
+    <>
+      <div className="flex items-center justify-start p-2.5 rounded-[5px] bg-white">
+        <div className="mr-2.5 flex items-center justify-center size-[60px] rounded-full bg-sky-800">
+          <FontAwesomeIcon
+            icon={faLocationDot}
+            className="text-white text-3xl"
+          />
+        </div>
+        <div>
+          <h5 className="uppercase font-bold text-sm font-redHat">
+            Company address
+          </h5>
+          <p className="text-[13px]">
+            {COMPANY_ADDRESS.map((el, ind) =>
+              ind === 0 ? (
+                <strong key={el} className="block">
+                  {el}
+                </strong>
+              ) : (
+                <span key={el} className="block">
+                  {el}
+                </span>
+              )
+            )}
+          </p>
+        </div>
+      </div>
+      <div className={`flex items-center justify-start p-2.5 bg-white`}>
+        <div className="mr-2.5 flex items-center justify-center size-[60px] rounded-full bg-sky-800">
+          <FontAwesomeIcon
+            icon={faDollarSign}
+            className="text-white text-3xl"
+          />
+        </div>
+        <div>
+          <h5 className="uppercase font-bold text-sm  font-redHat">
+            INVOICE DATA
+          </h5>
+          <p className="text-[13px]">
+            {INVOICE_DATA.map(({ item, value }) => (
+              <p key={item}>
+                <strong> {item}</strong>: <span> {value}</span>
+              </p>
+            ))}
+          </p>
+        </div>
+      </div>
+      <div className={`flex items-center justify-start p-2.5 bg-white`}>
+        <div className="mr-2.5 flex items-center justify-center size-[60px] rounded-full bg-sky-800">
+          <FontAwesomeIcon icon={faEnvelope} className="text-white text-3xl" />
+        </div>
+        <div>
+          <h5 className="uppercase font-bold text-sm font-redHat">CONTACTS</h5>
+          <p className="text-[13px]">
+            {EMAILS.map(({ item, value }) => (
+              <p key={item}>
+                <strong> {item}</strong>: <span> {value}</span>
+              </p>
+            ))}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ delicious-games/src/data/contactsData.ts
@@ -0,0 +1,20 @@
+export const COMPANY_ADDRESS = [
+  "Kateřina Suchá",
+  "Kettnerova 2051",
+  "15500 Prague",
+  "Czech Republic",
+];
+
+export const INVOICE_DATA = [
+  { item: "Company ID", value: "07084889" },
+  { item: "VAT number", value: "CZ7662130509" },
+  { item: "Bank account", value: "2511251126/5500" },
+  { item: "IBAN", value: "CZ8855000000002511251126" },
+  { item: "Swift code", value: "RZBCCZPP" },
+  { item: "Paypal", value: "info@deliciousgames.org" },
+];
+
+export const EMAILS = [
+  { item: "E-mail", value: "info@deliciousgames.org" },
+  { item: "E-mail for reviewers", value: "vojta@deliciousgames.org" },
+];
