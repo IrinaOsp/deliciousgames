0e70f1a5b64b878ebae53a9f99dca26cc5f92854
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-07-26 16:40:54 +0200

    add strapi to main page products slider

--- delicious-games/src/app/components/gamesSlider/productsSlider.tsx
+++ delicious-games/src/app/components/gamesSlider/productsSlider.tsx
@@ -1,32 +1,55 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Slider, { CustomArrowProps, Settings } from "react-slick";
-import Image from "next/image";
 import Link from "next/link";
 import "./productsSlider.css";
+import { getStrapiURL } from "@/utils/strapi";
+import qs from "qs";
+import { StrapiImage } from "../UI/StrapiImage/StrapiImage";
+import { Routes } from "@/data/data";
 
-const products = [
+const productsQuery = qs.stringify(
   {
-    boxImg: "/imgs/for-test.png",
-    title: "Evacuation",
+    populate: {
+      images: {
+        populate: {
+          box: {
+            fields: ["alternativeText", "url"],
+          },
+        },
+      },
+    },
   },
-  {
-    boxImg: "/imgs/for-test.png",
-    title: "Evacuation",
-  },
-  {
-    boxImg: "/imgs/for-test.png",
-    title: "Evacuation",
-  },
-  {
-    boxImg: "/imgs/Woodcraft box - 3D visualisation-350x350h.png",
-    title: "Woodcraft",
-  },
-  {
-    boxImg: "/imgs/Woodcraft box - 3D visualisation-350x350h.png",
-    title: "Woodcraft",
-  },
-];
+  { encodeValuesOnly: true }
+);
+
+type ResponseItem = {
+  id: number;
+  attributes: {
+    title: string;
+    path: string;
+    images: {
+      box: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+    };
+  };
+};
+
+type Response = {
+  data: ResponseItem[];
+};
+
+type Product = {
+  boxImg: string;
+  title: string;
+  path: string;
+};
 
 function SampleArrow(props: CustomArrowProps) {
   const { className, style, onClick } = props;
@@ -40,6 +63,34 @@ function SampleArrow(props: CustomArrowProps) {
 }
 
 export default function ProductsSlider() {
+  const [products, setProducts] = useState<Product[] | null>(null);
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const baseUrl = getStrapiURL();
+        const url = new URL("/api/games", baseUrl);
+        url.search = productsQuery;
+        console.log(url.href);
+        const res: Response = await fetch(url.href).then((res) => res.json());
+        if (res.data.length) {
+          const products: Product[] = res.data.map((el: any) => {
+            return {
+              boxImg: el.attributes.images.box.data.attributes.url,
+              title: el.attributes.title,
+              path: el.attributes.path,
+            };
+          });
+          setProducts(products);
+        }
+        return res;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getProducts();
+  }, []);
+
   const settings: Settings = {
     dots: true,
     slidesToShow: 4,
@@ -59,25 +110,31 @@ export default function ProductsSlider() {
       },
     ],
   };
+
   return (
     <div className="px-5">
       <Slider {...settings} className="">
-        {products.map((el) => (
-          <Link href={"/"} key={el.title} className="group">
-            <div>
-              <Image
-                src={el.boxImg}
-                alt={el.title}
-                className="w-full group-hover:scale-110 transition-all"
-                width={303}
-                height={303}
-              />
-              <p className="text-center uppercase text-[17px] font-bold group-hover:text-pink-600">
-                {el.title}
-              </p>
-            </div>
-          </Link>
-        ))}
+        {products &&
+          products.map((el) => (
+            <Link
+              href={`${Routes.CATALOGUE}/${el.path}`}
+              key={el.title}
+              className="group"
+            >
+              <div>
+                <StrapiImage
+                  src={el.boxImg}
+                  alt={el.title}
+                  className="w-full group-hover:scale-110 transition-all"
+                  width={303}
+                  height={303}
+                />
+                <p className="text-center uppercase text-[17px] font-bold group-hover:text-pink-600">
+                  {el.title}
+                </p>
+              </div>
+            </Link>
+          ))}
       </Slider>
     </div>
   );
