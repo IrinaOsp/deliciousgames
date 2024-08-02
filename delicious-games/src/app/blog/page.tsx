27cee07062d31d1b5daa62a7cc1d0dc0d2ea3bd0
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-08-02 14:45:31 +0200

    header and arrows fixes

---
+++ delicious-games/src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+import PageHeading from "../components/UI/pageHeading/pageHeading";
+
+export default function Blog() {
+  return (
+    <div className="max-w-7xl mx-auto p-5 xl:px-0">
+      <PageHeading title={"Blog"} />
+    </div>
+  );
+}
--- delicious-games/src/app/components/header/header.tsx
+++ delicious-games/src/app/components/header/header.tsx
@@ -2,37 +2,38 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import NavLinks from "./navLinks/navLinks";
 import LangSwitch from "./langSwitch/langSwitch";
 
 export default function Header() {
   const [isScrolled, setIsScrolled] = useState<boolean>(false);
+  const paths = usePathname().split("/");
+  const path = paths[1];
 
   const handleScroll = (): void => {
     setIsScrolled(window.scrollY > 50);
   };
 
   useEffect(() => {
+    if (path.length) {
+      setIsScrolled(true);
+      return;
+    }
     handleScroll();
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  const stickyHeaderStyle: React.CSSProperties = {
-    position: "sticky",
-    top: 0,
-    zIndex: 100,
-    backgroundColor: isScrolled ? "rgb(44, 54, 64)" : "transparent",
-    transition: "background-color 0.3s ease, color 0.3s ease",
-  };
+  }, [path]);
 
   return (
     <header
-      style={stickyHeaderStyle}
-      className={`bg-transparent w-full z-50 fixed top-0 ${
-        isScrolled ? "shadow-[0_5px_30px_-5px_rgba(0,0,0,0.15)]" : ""
-      }`}
+      className={`sticky w-full z-50 top-0 transition-colors duration-300 ease-in-out
+         ${
+           isScrolled
+             ? "bg-slate-800 shadow-[0_5px_30px_-5px_rgba(0,0,0,0.15)]"
+             : "transparent"
+         }`}
     >
       <div className="bg-slate-800 flex flex-wrap lg:flex-nowrap relative container mx-0 lg:mx-auto justify-start items-center min-w-full lg:min-w-max lg:max-w-7xl lg:gap-5 h-[85px] lg:h-[140px]">
         <div className="bg-white mt-auto mb-0 size-12 lg:h-full lg:w-[180px] inline-flex justify-center items-center">
