636a0a2eb593e1b8d8372c4449c71b77493ee658
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-08-13 09:32:39 +0200

    add translation for error page

--- delicious-games/src/app/[locale]/error.tsx
+++ delicious-games/src/app/[locale]/error.tsx
@@ -1,27 +1,32 @@
 "use client";
 
 import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 export default function Error({
   error,
   reset,
+  params: { locale },
 }: {
   error: Error & { digest?: string };
   reset: () => void;
+  params: { locale: string };
 }) {
   useEffect(() => {
     console.error(error);
   }, [error]);
 
+  const { t } = useTranslation("error");
+
   return (
     <div className="flex flex-col gap-3 justify-center items-center mt-3">
-      <h2 className="text-3xl">Something went wrong!</h2>
+      <h2 className="text-3xl">{t("smthWentWrong")}</h2>
       <p className="text-red-500">{"Error: " + error.message}</p>
       <button
         className="block px-2.5 py-1.5 bg-pink-600 text-white"
         onClick={() => reset()}
       >
-        Try again
+        {t("tryAgain")}
       </button>
     </div>
   );
